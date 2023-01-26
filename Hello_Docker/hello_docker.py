12f2a32c0eb3950ca9e041ab089e6b11ad335926
bavo.denys <bavodenys@gmail.com>
2023-01-26 19:22:48 +0100

    use argument parse to provide a argument in the docker container

--- Hello_Docker/hello_docker.py
+++ Hello_Docker/hello_docker.py
@@ -1,2 +1,19 @@
+import argparse
+
+def read_arguments():
+    parser = argparse.ArgumentParser(description='''Hello World''', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--name",
+                        type=str,
+                        required=True,
+                        help="Name")
+    return parser
+
+def say_hello_world(name):
+    print(f"Hello to {name} from the crazy Docker world")
+
+# Main section
 if __name__ == "__main__":
-	print('Hello Docker')
+    parser = read_arguments()
+    args = parser.parse_args()
+    name = args.name
+    say_hello_world(name)
